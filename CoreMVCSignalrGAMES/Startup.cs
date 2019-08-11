25c15bb8ab368160f6a641a9276fbe63d4eff5c0
puszynski <puszynski@gmail.com>
2019-08-11 14:34:23 +0200

    Add project files.

---
+++ CoreMVCSignalrGAMES/Startup.cs
@@ -0,0 +1,72 @@
+using CoreMVC.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+// TUTORIAL => https://docs.microsoft.com/pl-pl/aspnet/core/tutorials/signalr?view=aspnetcore-2.2&tabs=visual-studio
+namespace CoreMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+            });
+
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ CoreMVCSignalrGAMES/wwwroot/js/chat.js
@@ -0,0 +1,74 @@
+﻿"use strict";
+
+
+
+
+// Store user nick in browser memory
+var userNickInfo = document.getElementById('userNickInfo');
+//var setNickButton = document.getElementById('setNickButton');
+
+if (!localStorage.getItem('userNick')) {
+    userNickInfo.textContent = "Welcome on chat room! Please set your nick by clicking button below, we will save it in your browser memory";
+    createButtonToSetNick();
+    setNickButton.onclick = function () {
+        setUserNick();    
+    };
+}
+else {
+    var storedNick = localStorage.getItem('userNick');
+    userNickInfo.textContent = 'Hello ' + storedNick;
+}
+
+
+function setUserNick() {
+    var userNick = prompt('Please enter your nick.');
+    localStorage.setItem('userNick', userNick);
+    userNickInfo.textContent = 'Hello ' + userNick;
+    document.getElementById('setNickButton').style.display = "none";
+}
+
+function createButtonToSetNick() {
+    var setNickButton = document.createElement('button');
+    setNickButton.innerHTML = 'Set your nick';
+    setNickButton.setAttribute('id', 'setNickButton');
+    document.getElementById('setNickButtonArea').appendChild(setNickButton);
+}
+
+
+
+
+
+
+
+
+
+
+
+// SignalR 
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+// Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = storedNick; //document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
